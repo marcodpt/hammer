78b22bd8e25c9b3ee4673e1733222da61af832e1
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-11-18 08:43:17 -0300

    review examples

--- examples/superfine_counter.js
+++ examples/superfine_counter.js
@@ -4,7 +4,7 @@ const setState = state => superfine(
   document.getElementById("app"),
   ({main, h1, button, text}) => main({}, [
     h1({}, [
-      text(state)
+      text('Counter: '+String(state))
     ]),
     button({
       onclick: () => setState(state - 1)
--- examples/todo.js
+++ examples/todo.js
@@ -4,15 +4,6 @@ document.body.appendChild(dom(({
   main, h1, img, input, ul, li, button, text
 }) => 
   main({}, [
-    h1({}, [
-      img({
-        src: '../favicon.ico'
-      }),
-      text(' To do list')
-    ]),
-    input({
-      type: 'text'
-    }),
     ul(),
     button({
       onclick: ev => {
